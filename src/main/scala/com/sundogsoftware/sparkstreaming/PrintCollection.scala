a678b06c311d7f696bc6fe882ff951c885daf3bf
Juan Reyes Ponce <juan.reyes.ponce@ing.com>
2019-10-08 15:04:39 +0200

    Enhanced printCollection using Spark SQL

--- src/main/scala/com/sundogsoftware/sparkstreaming/PrintCollection.scala
+++ src/main/scala/com/sundogsoftware/sparkstreaming/PrintCollection.scala
@@ -1,19 +1,35 @@
 package com.sundogsoftware.sparkstreaming
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+case class NumberMapped(val number: java.lang.Integer) {
+
+  //Java friendly getter
+  def getNumber() = number
+}
 
 object PrintCollection {
 
+  import scala.collection.JavaConversions._
+
+  private def readLocalData(implicit spark: SparkSession) : DataFrame = {
+    val numbers = seqAsJavaList(Array(NumberMapped(1), NumberMapped(2)))
+    spark.createDataFrame(numbers, classOf[NumberMapped])
+  }
 
   def main(args: Array[String]): Unit = {
 
-    val conf : SparkConf = new SparkConf().setAppName("printNumbers").setMaster("local[*]")
-    val sc : SparkContext = new SparkContext(conf)
+    implicit val spark = SparkSession
+      .builder
+      .appName("PrintCollection")
+      .master("local[*]")
+      //.config("spark.sql.warehouse.dir", "file:///C:/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
+      .config("spark.sql.streaming.checkpointLocation", "./checkpoint")
+      .getOrCreate()
 
-    val numbers = Array(1,2,3,4,5)
+    val numbersTable = readLocalData.createOrReplaceTempView("numbers")
+    val df: DataFrame = spark.sql("SELECT * FROM numbers WHERE number == 1")
 
-    val distNumbers = sc.parallelize(numbers)
-
-    println(distNumbers.reduce(_+_))
+    df.show()
   }
 }
